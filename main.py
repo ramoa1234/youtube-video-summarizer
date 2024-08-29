98b03cb7eb6eb60265c282693f3e8e47523d34d6
Antonio Ramos <117685319+ramoa1234@users.noreply.github.com>
2024-08-29 16:57:53 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from openai import OpenAI
+
+test_video_id = "rXld2YqnEgY"
+
+
+
+def get_summary(ai_question):
+    client = OpenAI(
+    api_key= "enter API key here",
+    organization="org-U1OqjvSUtE2t3gwXcoH0DuMI"
+
+    )
+
+
+
+    #pass int eh 
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "you are a helpful assiant can you please summarize what this text is about"},
+            {"role": "user", "content": ai_question}
+        ]
+    )
+
+    message = response.choices[0].message.content
+    print(message)
+
+
+def main():
+    video_id = input("'input the video id here: ") 
+    transcript = YouTubeTranscriptApi.get_transcript(video_id) 
+
+    ai_question = ""
+    for key in transcript:
+        x = key.get('text')
+        ai_question += x
+    get_summary(ai_question)
+
+main()
